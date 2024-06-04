c50c00b8b1761b6cb78ec8d981952f28d1f7ccbb
Proud of JW <154409699+oncliff-climbing@users.noreply.github.com>
2024-06-04 10:21:44 +0900

    Update main.py

--- back-app/main.py
+++ back-app/main.py
@@ -1,11 +1,10 @@
 import traceback
 from fastapi import FastAPI, HTTPException, WebSocket
-import mysql.connector
-from fastapi.responses import FileResponse
-from pydantic import BaseModel
-from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import mysql.connector
+from mysql.connector import pooling
 import subprocess
 import logging
 import asyncio
@@ -15,13 +14,13 @@ app = FastAPI()
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 허용할 도메인 리스트. 모든 도메인을 허용하려면 ["*"] 사용.
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # 허용할 HTTP 메서드 리스트. 모든 메서드를 허용하려면 ["*"] 사용.
-    allow_headers=["*"],  # 허용할 HTTP 헤더 리스트. 모든 헤더를 허용하려면 ["*"] 사용.
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# DB
+# DB 연결 풀 설정
 db_config = {
     'user': 'root',
     'password': 'test1234',
@@ -29,8 +28,11 @@ db_config = {
     'database': 'api'
 }
 
-conn = mysql.connector.connect(**db_config)
-cursor = conn.cursor()
+connection_pool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="mypool",
+    pool_size=10,
+    **db_config
+)
 
 # 데이터를 전송하기 위한 모델 정의
 class TestData(BaseModel):
@@ -72,16 +74,23 @@ async def read_root():
 @app.get('/testcase')
 async def read_list():
     try:
+        conn = connection_pool.get_connection()
+        cursor = conn.cursor()
         cursor.execute("SELECT * FROM test")
         result = cursor.fetchall()
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    finally:
+        cursor.close()
+        conn.close()
 
 # 테스트 생성
 @app.post('/testcase')
 async def create_test(data: TestData):
     try:
+        conn = connection_pool.get_connection()
+        cursor = conn.cursor()
         cursor.execute(
             """
             INSERT INTO test (target_url, test_name, user_num, user_plus_num, interval_time, plus_count)
@@ -94,21 +103,30 @@ async def create_test(data: TestData):
         return {"test_id": test_id, "test_name": data.test_name}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    finally:
+        cursor.close()
+        conn.close()
 
 # 테스트 삭제
 @app.delete("/testcase/{test_id}")
 async def delete_test(test_id: int):
     try:
+        conn = connection_pool.get_connection()
+        cursor = conn.cursor()
         cursor.execute("DELETE FROM test WHERE test_id = %s", (test_id,))
         conn.commit()
         return {"message": "Test deleted successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    finally:
+        cursor.close()
+        conn.close()
 
 # 테스트 실행
 @app.get("/testcase/{test_id}/execute/")
 async def execute_test(test_id: int):
     try:
+        conn = connection_pool.get_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM test WHERE test_id = %s", (test_id,))
         test_data = cursor.fetchone()
@@ -128,17 +146,18 @@ async def execute_test(test_id: int):
             raise HTTPException(status_code=404, detail="Testcase not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    finally:
+        cursor.close()
+        conn.close()
     
 # 테스트 결과값 반환
 @app.get("/testcase/{test_id}/stats/")
 async def stats(test_id: int):
     try:
-        conn = mysql.connector.connect(**db_config)
+        conn = connection_pool.get_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT MAX(count) FROM incremental WHERE test_id = %s", (test_id,))
         count = cursor.fetchone()[0]
-        print("count: ", count)
-        print("count: ",count)
         cursor.execute("SELECT * FROM incremental WHERE test_id = %s and count = %s", (test_id, count))
         updated_stats = cursor.fetchall()
         if updated_stats:
@@ -147,60 +166,57 @@ async def stats(test_id: int):
             raise HTTPException(status_code=404, detail="No updated stats found for this test_id")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    finally:
+        cursor.close()
+        conn.close()
 
 # 테스트 결과값 반환
 @app.get("/testcase/{test_id}/spike-stats/")
 async def pre_stats(test_id: int):
     try:
+        conn = connection_pool.get_connection()
+        cursor = conn.cursor()
         cursor.execute("SELECT * FROM spike WHERE test_id = %s", (test_id,))
         updated_stats = cursor.fetchall()
-        print(updated_stats)
         if updated_stats:
             return updated_stats
         else:
             raise HTTPException(status_code=404, detail="No updated stats found for this test_id")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
-    
-# 테스트 결과 삭제
-"""
-@app.delete("/testcase/{test_id}/stats/")
-async def delete_test(test_id: int):
-    try:
-        cursor.execute("DELETE FROM incremental WHERE test_id = %s", (test_id,))
-        conn.commit()
-        return {"message": "Test Result deleted successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
-"""
+    finally:
+        cursor.close()
+        conn.close()
+
 @app.get("/testcase/{id}/results")
 async def get_id_list(id: int):
     try:
-        conn = mysql.connector.connect(**db_config)
+        conn = connection_pool.get_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT count FROM incremental WHERE test_id = %s", (id,))
         idList = cursor.fetchall()
         idList = set(idList)
-        print(idList)
         idList = [t[0] for t in idList]
         idList.sort()
-        print(idList)
         if idList:
             return idList
     except Exception as e:
-            raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    finally:
+        cursor.close()
+        conn.close()
 
-# 비교할 테스트  결과값 반환
+# 비교할 테스트 결과값 반환
 @app.get("/testcase/{test_id}/stats/{selectedResult}")
 async def stats(test_id: int, selectedResult: int):
     try:
-        conn = mysql.connector.connect(**db_config)
+        conn = connection_pool.get_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM incremental WHERE test_id = %s AND count = %s", (test_id, selectedResult,))
         test_cases = cursor.fetchall()
-        print(test_cases)
         return test_cases
     except Exception as e:
-        # traceback.print_exc()
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
-
+    finally:
+        cursor.close()
+        conn.close()
